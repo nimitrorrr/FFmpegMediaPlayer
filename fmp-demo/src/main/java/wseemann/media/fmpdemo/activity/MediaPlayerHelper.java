965d7f5d88c0080bb0de1d36cf55200a16b2f54d
nimitrorrr <senya.vileyskiy@gmail.com>
2025-09-14 07:14:17 +0300

    Update MediaPlayerHelper.java

--- fmp-demo/src/main/java/wseemann/media/fmpdemo/activity/MediaPlayerHelper.java
+++ fmp-demo/src/main/java/wseemann/media/fmpdemo/activity/MediaPlayerHelper.java
@@ -1,79 +1,24 @@
-package wseemann.media.fmpdemo.helper;
-
-import android.util.Log;
-import android.view.Surface;
-
-import java.io.IOException;
-
-import wseemann.media.FFmpegMediaPlayer;
-
-public class MediaPlayerHelper implements 
-        FFmpegMediaPlayer.OnPreparedListener,
-        FFmpegMediaPlayer.OnErrorListener {
-
-    private FFmpegMediaPlayer mMediaPlayer;
-    private Surface mSurface;
-    private String mDataSource;
-
-    public void setSurface(Surface surface) {
-        mSurface = surface;
-        try {
-            tryToPrepare();
-        } catch (IOException e) {
-            Log.e("MediaPlayerHelper", "Error preparing player when setting surface", e);
-        }
+// Добавим эти методы в класс MediaPlayerHelper
+public void pause() {
+    if (mMediaPlayer != null) {
+        mMediaPlayer.pause();
     }
+}
 
-    public void setDataSource(String path) throws IOException {
-        mDataSource = path;
-        tryToPrepare();
-    }
+public boolean isPlaying() {
+    return mMediaPlayer != null && mMediaPlayer.isPlaying();
+}
 
-    private void tryToPrepare() throws IOException {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.reset();
-        } else {
-            mMediaPlayer = new FFmpegMediaPlayer();
-            mMediaPlayer.setOnPreparedListener(this);
-            mMediaPlayer.setOnErrorListener(this);
-        }
+public long getCurrentPosition() {
+    return mMediaPlayer != null ? mMediaPlayer.getCurrentPosition() : 0;
+}
 
-        if (mDataSource != null) {
-            mMediaPlayer.setDataSource(mDataSource);
-            if (mSurface != null) {
-                mMediaPlayer.setSurface(mSurface);
-            }
-            mMediaPlayer.prepareAsync();
-        }
-    }
+public long getDuration() {
+    return mMediaPlayer != null ? mMediaPlayer.getDuration() : 0;
+}
 
-    public void start() {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.start();
-        }
-    }
-
-    public void stop() {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.stop();
-        }
-    }
-
-    public void release() {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-        }
-    }
-
-    @Override
-    public void onPrepared(FFmpegMediaPlayer mp) {
-        mp.start();
-    }
-
-    @Override
-    public boolean onError(FFmpegMediaPlayer mp, int what, int extra) {
-        Log.d("MediaPlayerHelper", "Error: " + what);
-        return true;
+public void seekTo(long position) {
+    if (mMediaPlayer != null) {
+        mMediaPlayer.seekTo(position);
     }
 }
